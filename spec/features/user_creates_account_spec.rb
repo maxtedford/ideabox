88c1c4540dda57234e749c25eeea83303aca2760
Chelsea Worrel <chelseaworrel@gmail.com>
2015-06-11 16:02:58 -0600

    add account creation functionality for user

---
+++ spec/features/user_creates_account_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+describe "user creates account", type: :feature do
+
+  it "presents an unregistered user with a welcome page" do
+  visit users_path
+
+  assert page.has_content?("Welcome to IdeaBox")
+  assert page.has_link?("Create Account")
+  assert page.has_link?("Login")
+  end
+
+  it "creates a new account" do
+  visit users_path
+  click_link("Create Account")
+  fill_in "Username", with: "tswift"
+  fill_in "Password", with: "password"
+  click_button "Create Account"
+
+  assert page.has_content?("Welcome tswift")
+  expect(User.all.count).to eq(1)
+  reset_session!
+  end
+
+end
+
+
+# As a User...
+#
+# When I visit the root path, click "Create New Account",
+# then fill in the form and click "Create Account",
+#
+# I expect to be presented with my '/show' page which contains all my ideas.
