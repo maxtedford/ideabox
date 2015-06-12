b31fc215aa75904f3f9fab7a9415100cf71226d5
Max Tedford <max.tedford@gmail.com>
2015-06-11 20:47:59 -0600

    add validation tests for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ActiveRecord::Base
   has_secure_password
   # has_many :ideas
+  
+  validates :username,
+    presence: true,
+    uniqueness: true
+  validates :password,
+    presence: true
 end
