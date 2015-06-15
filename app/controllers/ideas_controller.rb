b58ac0049b0f1618e7a63900e53e72b6b821a89f
Max Tedford <max.tedford@gmail.com>
2015-06-14 20:59:36 -0600

    add ideas having categories

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -40,6 +40,6 @@ class IdeasController < ApplicationController
   private
   
   def idea_params
-    params.require(:idea).permit(:title, :body, :user_id)
+    params.require(:idea).permit(:title, :body, :user_id, :category_id)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150614163409) do
+ActiveRecord::Schema.define(version: 20150614234453) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -33,8 +33,9 @@ ActiveRecord::Schema.define(version: 20150614163409) do
     t.string   "title"
     t.string   "body"
     t.integer  "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.integer  "category_id"
   end
 
   add_index "ideas", ["user_id"], name: "index_ideas_on_user_id"
@@ -52,4 +53,5 @@ ActiveRecord::Schema.define(version: 20150614163409) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
   end
+
 end
